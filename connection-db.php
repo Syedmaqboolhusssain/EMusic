c53a7c7454318392fe630d5ea6fb0022b41640ac
Syedmaqboolhussain <syedmaqboolhussain93@gmail.com>
2016-03-29 12:38:45 +0500

    Update connection-db.php

--- connection-db.php
+++ connection-db.php
@@ -1,11 +1,12 @@
  <?php
-$error="Connection error";
-$host='sql6.freemysqlhosting.net';
-$user='sql6112634';
-$pass='9zdQqiq6ml';
-$db=' sql6112634';
-$port='3306';
-$connect=mysqli_connect($host,$user,$pass,$db,$port);
+// $error="Connection error";
+// $host='sql6.freemysqlhosting.net';
+// $user='sql6112634';
+// $pass='9zdQqiq6ml';
+// $db=' sql6112634';
+// $port='3306';
+// $connect=mysqli_connect($host,$user,$pass,$db,$port);
+$connect=mysqli_connect("sql6.freemysqlhosting.net","sql6112634","9zdQqiq6ml","sql6112634","3306");
 	if($connect){
 		}
 	else echo $error;
