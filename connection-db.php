79e28bec9e7f3fffb430072786c18b28ea9231b9
Syedmaqboolhussain <syedmaqboolhussain93@gmail.com>
2016-03-29 12:31:50 +0500

    Update connection-db.php

--- connection-db.php
+++ connection-db.php
@@ -1,17 +1,11 @@
  <?php
 $error="Connection error";
-$host='sql6.freemysqlhosting.net';
-$user='sql6112634';
-$pass='9zdQqiq6ml';
-$db='sql6112634';
-try {
-    $connect = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    // set the PDO error mode to exception
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+$host='localhost';
+$user='root';
+$pass='';
+$db='iad';
+$connect=mysqli_connect($host,$user,$pass,$db);
+	if($connect){
+		}
+	else echo $error;
 ?>
