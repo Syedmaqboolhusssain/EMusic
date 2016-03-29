faa920e48fcd0e40ae0082c4480a65f820967877
Syedmaqboolhussain <syedmaqboolhussain93@gmail.com>
2016-03-29 13:22:32 +0500

    Update header.php

--- header.php
+++ header.php
@@ -10,10 +10,7 @@ if(!isset($_SESSION['user_name'])){
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-tap-highlight" content="no">
-    <!--<meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">-->
-    <!--<meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">-->
-    <title>E-Music</title>
+     <title>E-Music</title>
 
     <!-- Favicons-->
     <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
